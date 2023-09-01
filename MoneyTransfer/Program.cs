96085336d6be5f803905088d082e08aa7823beaa
mussi <mussiah520@gmail.com>
2023-08-31 19:21:20 -0500

    Add project files.

---
+++ MoneyTransfer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MoneyTransfer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Simulation sim = new Simulation();
+            sim.RunSimulation();
+
+
+        }
+    }     
+    
+
+         
+}
